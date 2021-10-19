0a188781a91ce64899f3e4194f9465f3451078d6
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2021-10-19 12:20:33 +0500

    define userschema

---
+++ server/database/conn.js
@@ -0,0 +1,14 @@
+const dotenv=require('dotenv')
+dotenv.config({path:'./config.env'})
+const mongoose=require('mongoose')
+const db=process.env.DATABASE
+mongoose.connect(db,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+ }).then(()=>
+ console.log("conneted to the database"))
+ .catch((err)=>{
+     console.log("no connection")
+ })
