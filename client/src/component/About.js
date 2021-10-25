cd5969b71a047e1c5745239458446ea8ee38fc43
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2021-10-25 22:12:09 +0500

    2nd commit

--- client/src/component/About.js
+++ client/src/component/About.js
@@ -1,9 +1,70 @@
 import React from 'react'
 
+
 function About() {
     return (
         <div>
-            This is about
+          <h2>About Me</h2>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>   
+
+	<div class="carousel-inner">		
+		<div class="carousel-item active">
+		
+			<p class="testimonial">I am a B.tech C.S.E student at NIT jamshedpur.I completed my 12th from DAV public school</p>
+			<p class="overview"><b>Subham Kumar</b>MERN stack developer at <a href="#">NIT jamshedpur</a></p>
+			<div class="star-rating">
+				<ul class="list-inline">
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+				</ul>
+			</div>
+		</div>
+		<div class="carousel-item">
+	
+			<p class="testimonial">I am intrested in MERN stack development.I have worked in technologies like Reactjs ,Nodejs,Express js,Mongo DB</p>
+			<p class="overview"><b>Subham Kumar</b>MERN stack developer at  <a href="#">NIT jamshedpur</a></p>
+			<div class="star-rating">
+				<ul class="list-inline">
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+				</ul>
+			</div>
+		</div>
+		<div class="carousel-item">
+
+			<p class="testimonial">I am currently in 2nd year and pursuing btech 4 year Btech degree</p>
+			<p class="overview"><b>Subham Kumar</b>Web Developer at <a href="#">Web Team</a></p>
+			<div class="star-rating">
+				<ul class="list-inline">
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star"></i></li>
+					<li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
+				</ul>
+			</div>
+		</div>		
+	</div>
+
+	<a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+		<i class="fa fa-angle-left"></i>
+	</a>
+	<a class="carousel-control-next" href="#myCarousel" data-slide="next">
+		<i class="fa fa-angle-right"></i>
+	</a>
+</div>
         </div>
     )
 }
--- client/src/component/Sginup.js
+++ client/src/component/Sginup.js
@@ -1,62 +1,85 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
+import { NavLink } from 'react-router-dom'
 function Sginup() {
     return (
         <>
-          <section className='signup'>
-              <div className='container mt-5'>
-                  <div className='signup-content'>
-                      <div className='signup-form'>
-                           <h2 className='form-title'>Sign up</h2>
-                           <form className='register-form' id='register-form'>
-                                <div className='form-group'>
-                                        <label htmlFor='name'>
-                                        <i class="zmdi zmdi-assignment-account"></i>  
-                                        </label>
-                                        <input type='text' name='name' id='name'autoComplete='off'placeholder='your name'/>
-                                </div>
-                                <div className='form-group'>
-                                        <label htmlFor='email'>
-                                        <i class="zmdi zmdi-email"></i> 
-                                        </label>
-                                        <input type='text' name='email' id='email'autoComplete='off'placeholder='your email'/>
-                                </div>
-                                <div className='form-group'>
-                                        <label htmlFor='Phone'>
-                                        <i class="zmdi zmdi-phone"></i>  
-                                        </label>
-                                        <input type='text' name='phone' id='phone'autoComplete='off'placeholder='your phone'/>
-                                </div>
-                                <div className='form-group'>
-                                        <label htmlFor='work'>
-                                        <i class="zmdi zmdi-slideshow"></i>  
-                                        </label>
-                                        <input type='text' name='work' id='work'autoComplete='off'placeholder='your work'/>
-                                </div>
-                                <div className='form-group'>
-                                        <label htmlFor='password'>
-                                        <i class="zmdi zmdi-lock"></i>  
-                                        </label>
-                                        <input type='password' name='password' id='password'autoComplete='off'placeholder='your password'/>
-                                </div>
+            <div className="signup-form">
+                <form action="/examples/actions/confirmation.php" method="post" />
+                <h2>Sign Up</h2>
+                <p>Please fill in this form to create an account!</p>
+                <hr />
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                            <i class="zmdi zmdi-assignment-account"></i>
+                            </span>
+                        </div>
+                        <input type="text" className="form-control" name="username" placeholder="Username" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                            <i class="zmdi zmdi-account"></i>
+                            </span>
+                        </div>
+                        <input type="email" className="form-control" name="email" placeholder="Email Address" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                            <i class="zmdi zmdi-phone"></i>
+                            </span>
+                        </div>
+                        <input type="number" className="form-control" name="mobile" placeholder="Phone Number" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                            <i class="zmdi zmdi-slideshow"></i>
+                            </span>
+                        </div>
+                        <input type="text" className="form-control" name="work" placeholder="Your Profession" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                            <i class="zmdi zmdi-lock"></i>
+                            </span>
+                        </div>
+                        <input type="text" className="form-control" name="password" placeholder="Password" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                            <i class="zmdi zmdi-lock"></i>
+                                <i className="fa fa-check"></i>
+                            </span>
+                        </div>
+                        <input type="text" className="form-control" name="cpassword" placeholder="Confirm Password" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="form-check-label"><input type="checkbox" required="required" /> I accept the <NavLink to="/login">Terms of Use</NavLink> &amp; <NavLink to="/login">Privacy Policy</NavLink></label>
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary btn-lg">Sign Up</button>
+                </div>
 
-                                <div className='form-group'>
-                                        <label htmlFor='Confrim password'>
-                                        <i class="zmdi zmdi-lock"></i>  
-                                        </label>
-                                        <input type='password' name='password' id='password'autoComplete='off'placeholder='Confrim password'/>
-                                </div>
-                                <div className = 'form-group form-button'>
-                                    <input type='submit' name='signup' id ='signup' className='form-submit'/>
+                <div className="text-center">Already have an account? <NavLink to="/login">Login here</NavLink></div>
 
-                                </div>
-                           </form>
-                      </div>
+            </div>
 
-                  </div>
-
-              </div>
-        </section> 
         </>
     )
 }
