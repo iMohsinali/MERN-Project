39682a684e22018e4c0c9f38169cdbf6ac938648
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-02-13 16:03:45 +0500

    first commit to reveiw app

--- Reveiw_app/src/App.js
+++ Reveiw_app/src/App.js
@@ -2,9 +2,17 @@ import { Review } from "./Review";
 
 function App() {
   return (
-    <div className="App">
-      <Review />
-    </div>
+    <main>
+      <section className="App">
+        <div>
+          <h1>Reveiw App</h1>
+          <hr className="underline"></hr>
+        </div>
+      </section>
+      <div className="review">
+        <Review />
+      </div>
+    </main>
   );
 }
 
--- Reveiw_app/src/Review.jsx
+++ Reveiw_app/src/Review.jsx
@@ -1,5 +1,60 @@
 import React from "react";
-
+import people from "./data";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useState } from "react";
 export const Review = () => {
-  return <div>Review</div>;
+  const [index, setindex] = useState(0);
+  const { id, name, job, image, text } = people[index];
+  const check = (n) => {
+    if (n < 0) {
+      n = people.length - 1;
+      return n;
+    }
+    if (n > people.length - 1) {
+      n = 0;
+      return n;
+    } else {
+      return n;
+    }
+  };
+  const right = () => {
+    setindex((index) => {
+      let newindex = index + 1;
+      return check(newindex);
+    });
+  };
+  const left = () => {
+    setindex((index) => {
+      let newindex = index - 1;
+      return check(newindex);
+    });
+  };
+  const random = () => {
+    let newindex = Math.random() * people.length;
+    newindex = Math.floor(newindex);
+    if (newindex === index) {
+      newindex = index + 1;
+    }
+    setindex(check(newindex));
+  };
+  return (
+    <article>
+      <div className="image-container">
+        <img src={image} alt={name} className="person-image" />
+      </div>
+      <div className=" btn">
+        <button className="arrow-btn">
+          <IoIosArrowBack onClick={left} />
+        </button>
+        <span>
+          <button className="arrow-btn">
+            <IoIosArrowForward onClick={right} />
+          </button>
+        </span>
+      </div>
+      <div className="random" onClick={random}>
+        <button>random</button>
+      </div>
+    </article>
+  );
 };
