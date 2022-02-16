a26aa1f19b440296f9753bde1e9ecc99d57f54d6
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-02-16 22:48:19 +0500

    Qns complete

--- QnS_app/src/Question.js
+++ QnS_app/src/Question.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { AddCircleRounded, RemoveCircle } from "@material-ui/icons";
+import React, { useState } from "react";
 
-const Question = () => {
-  return <div>Question</div>;
+const Question = ({ title, info }) => {
+  const [show, setshow] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setshow(!show)}>
+          {show ? (
+            <RemoveCircle className="minus" />
+          ) : (
+            <AddCircleRounded className="plus" />
+          )}
+        </button>
+      </header>
+      {show && <p> {info}</p>}
+    </article>
+  );
 };
 
 export default Question;
